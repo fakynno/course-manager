8d53afb035566b348ccd7a8481b014dd42cf1b80
fakynno <fakynno@gmail.com>
2021-09-08 22:38:12 -0300

    criacao de objetos para exibir na view

--- src/app/courses/course-list.component.ts
+++ src/app/courses/course-list.component.ts
@@ -1,9 +1,38 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
+import { Course } from "./course";
 
 @Component ({
   selector: 'app-course-list',
   templateUrl: './course-list.component.html'
 })
-export class CourseListComponent {
+export class CourseListComponent implements OnInit {
+
+  courses: Course[] = [];
+
+  ngOnInit(): void  {
+    this.courses = [
+      {
+        id: 1,
+        name: 'Python na Prática',
+        imageUrl: '',
+        price: 12.54,
+        code: 'XPS-1243',
+        duration: 240,
+        rating: 3.1,
+        releaseDate: 'September, 8, 2021'
+      },
+      {
+        id: 2,
+        name: 'Java na Prática',
+        imageUrl: '',
+        price: 22.54,
+        code: 'XPS-6221',
+        duration: 120,
+        rating: 4.0,
+        releaseDate: 'September, 8, 2021'
+      }
+    ]
+  }
+
 
 }
